f712b9f64b64ddd00548740e623c3e185200c1aa
icezeros <icezeros@163.com>
2019-01-22 00:54:09 +0800

    test

---
+++ test.js
@@ -0,0 +1,24 @@
+const shell = require('shelljs');
+const { exec } = shell;
+const version = exec('node --version', { silent: true }).stdout;
+
+// const child = exec('yarn add lodash', { async: true });
+const child = exec('/home/pi/daqhats/examples/python/mcc118/single_value_read.py', { async: true });
+
+// child.code.on('data', function(data) {
+//   /* ... do something with data ... */
+// });
+child.stdout.on('data', function(data) {
+  /* ... do something with data ... */
+  console.log('===================Program output:', data);
+});
+child.stderr.on('data', function(data) {
+  /* ... do something with data ... */
+  console.log('===================Program stderr:', data);
+});
+
+// exec('node -v', function(code, stdout, stderr) {
+//   console.log('Exit code:', code);
+//   console.log('Program output:', stdout);
+//   console.log('Program stderr:', stderr);
+// });
