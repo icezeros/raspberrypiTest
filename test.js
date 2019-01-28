68c449aae3488dbd326fd9cc7c3af19d02731e61
icezeros <icezeros@163.com>
2019-01-28 22:15:10 +0800

    test

--- test.js
+++ test.js
@@ -2,6 +2,11 @@ const shell = require('shelljs');
 const path = require('path');
 const { exec } = shell;
 const version = exec('node --version', { silent: true }).stdout;
+let obj = {
+  ch0: 0,
+  ch1: 0,
+  index: 0,
+};
 
 // const child = exec('yarn add lodash', { async: true });
 const child = exec(path.join(__dirname, 'index.py'), { async: true });
@@ -18,6 +23,11 @@ child.stdout.on('data', function(data) {
     // console.log('============ arr =============');
     // console.log(arr);
     const i = (arr[0] - arr[1]) * 1000;
+    obj.ch0 += arr[0] * 1000;
+    obj.ch1 += arr[1] * 1000;
+    obj.index += 1;
+    sum();
+
     console.log('============ i =============');
     console.log(i);
   } catch (error) {
@@ -33,8 +43,18 @@ child.stderr.on('data', function(data) {
   console.log('===================Program stderr:', data);
 });
 
-// exec('node -v', function(code, stdout, stderr) {
-//   console.log('Exit code:', code);
-//   console.log('Program output:', stdout);
-//   console.log('Program stderr:', stderr);
-// });
+function sum() {
+  if (obj.index == 100) {
+    console.log('============ obj.ch0 =============');
+    console.log(obj.ch0);
+    console.log('============ obj.ch1 =============');
+    console.log(obj.ch1);
+    console.log('============ result  =============');
+    console.log((obj.ch0 - obj.ch1) / 100);
+    obj = {
+      ch0: 0,
+      ch1: 0,
+      index: 0,
+    };
+  }
+}
